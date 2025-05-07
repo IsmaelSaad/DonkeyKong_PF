e268d3b1943547872487a74bb01b60404b16c00d
ISMAPC\isma treball <a24ismsaajdi@inspedralbes.cat>
2025-05-07 17:29:43 +0200

    Commit de guardado.

--- Assets/Scripts/Escaleras/EscalerasController.cs
+++ Assets/Scripts/Escaleras/EscalerasController.cs
@@ -8,8 +8,8 @@ using UnityEngine.UIElements;
 public class EscalerasController : MonoBehaviour
 {
     
-    [Range(0, 10)]
-    public int escaleraLength;
+    [Range(0f, 10f)]
+    public float escaleraLength;
     [SerializeField]
     public GameObject escalera;
     [SerializeField]
@@ -39,11 +39,12 @@ public class EscalerasController : MonoBehaviour
 
         entrar.offset = new Vector3(entrar.offset.x, -0.05f, 0);
         salir.offset = new Vector3(entrar.offset.x, ((stairHeight * escaleraLength)) - stairHeight / 4, 0);
+        /* DESCOMENTAR PARA VISUALIZAR ESCALERAS
         for (int i = 0; i < escaleraLength; i++)
         {
             Vector3 position = new Vector3(transform.position.x, transform.position.y + (escaleraSprite.sprite.rect.height / escaleraSprite.sprite.pixelsPerUnit) * i, 1);
             Instantiate(escalera, position, Quaternion.identity, transform);
-        }
+        }*/
 
         middle.offset = new Vector3(middle.offset.x, (stairHeight*escaleraLength)/2 - 0.05f);
         middle.size = new Vector3(middle.size.x, stairHeight * escaleraLength - 0.1f);
