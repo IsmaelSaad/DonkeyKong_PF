2aae1375a567d0e1baf7890dc968ad8b60e4b2dc
ISMAPC\isma treball <a24ismsaajdi@inspedralbes.cat>
2025-05-14 23:10:29 +0200

    Cambios para implementar la deteccion de salto del barril.

--- Assets/Scripts/Enemies/Barrels/BarrelController.cs
+++ Assets/Scripts/Enemies/Barrels/BarrelController.cs
@@ -6,10 +6,12 @@ public class BarrelController : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] public float speed;
-    [SerializeField] BoxCollider2D detectStair;
+    [SerializeField] BoxCollider2D detectStair, playerPointsColl;
     [SerializeField] float bounceForce;
     [SerializeField] float groundRayDistance = 2.0f, stairRayDistance = 2.0f;
     [SerializeField] LayerMask groundMask;
+
+    GameManager gameManager;
 
     Rigidbody2D rb;
     BoxCollider2D boxColl;
@@ -19,10 +21,11 @@ public class BarrelController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = FindObjectOfType<GameManager>();
         rb = GetComponent<Rigidbody2D>();
         boxColl = GetComponentInChildren<BoxCollider2D>();
         animator = GetComponent<Animator>();
-        barrel = new Barrel(transform, animator, speed, rb, boxColl, detectStair, bounceForce, groundRayDistance, stairRayDistance, groundMask);
+        barrel = new Barrel(transform, animator, speed, rb, boxColl, playerPointsColl, detectStair, bounceForce, groundRayDistance, stairRayDistance, groundMask, gameManager);
     }
 
     void FixedUpdate()
@@ -32,7 +35,9 @@ public class BarrelController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        barrel.PointsOnTriggerEnter2D(collision);
         barrel.BarrelOnTriggerEnter2D(collision);
+        
 
         if (collision.CompareTag("OilBarrel"))
         {
@@ -43,5 +48,6 @@ public class BarrelController : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         barrel.BarrelOnTriggerExit2D(collision);
+        barrel.PointsOnTriggerExit2D(collision);
     }
 }
