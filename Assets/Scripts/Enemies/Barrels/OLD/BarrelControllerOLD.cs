8d592af3ebb7b133b7783a64dff3218e1863cae1
PolBelRos <a24polbelros@inspedralbes.cat>
2025-05-15 16:10:53 +0200

    Añadido la escena del game over y start de main menu funcional

--- Assets/Scripts/Enemies/Barrels/OLD/BarrelControllerOLD.cs
+++ Assets/Scripts/Enemies/Barrels/OLD/BarrelControllerOLD.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class BarrelControllerOLD : MonoBehaviour
 {
--- Assets/Scripts/UI/MenuActions.cs
+++ Assets/Scripts/UI/MenuActions.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.SceneManagement;
 
 public class MenuActions : MonoBehaviour
 {
-    [SerializeField] GameObject Title;
-    [SerializeField] GameObject DK;
-    [SerializeField] GameObject Start;
-    [SerializeField] InputActionAsset inputActionMenu;
+    int arrowIndex;
 
-    public InputAction anyKey, menuKey;
+    [SerializeField] GameObject Title, DK, Start, Selection;
+    [SerializeField] InputActionAsset inputActionMenu;
+    [SerializeField] RectTransform selectMode;
+    [SerializeField] RectTransform[] arrowPositions;
+    [SerializeField] bool isGameOver;
+
+    public InputAction anyKey, menuKey, menuSelect;
 
     enum State {
         PRESSTART, MENU
@@ -24,15 +28,32 @@ public class MenuActions : MonoBehaviour
         inputActionMenu.Enable();
         anyKey = inputActionMenu.FindActionMap("PressAnyKey").FindAction("AnyKeyButton");
         menuKey = inputActionMenu.FindActionMap("VerticalMenu").FindAction("MenuMovement");
+        menuSelect = inputActionMenu.FindActionMap("VerticalMenu").FindAction("SelectionMenu");
+
+        arrowIndex = 0;
+
+        if (isGameOver) state = State.MENU;
+        else 
+        {
+            DK.SetActive(true);
+            Start.SetActive(true);
+
+            Selection.SetActive(false);
+        }
     }
 
     private void Update()
     {
         switch (state) { 
             case State.PRESSTART:
-                PressAnyKey();
+                if (!isGameOver)
+                {
+                    PressAnyKey();
+                }
                 break;
             case State.MENU:
+                PressKeysMenu();
+                Selection.SetActive(true);
                 break;
         }
     }
@@ -50,9 +71,41 @@ public class MenuActions : MonoBehaviour
 
     void PressKeysMenu ()
     {
-        if (menuKey.ReadValue<float>() ) {
-
+        if (menuKey.ReadValue<float>() < 0 && menuKey.triggered) 
+        {
+            arrowIndex++;
+            if (arrowIndex > arrowPositions.Length - 1)
+            {
+                arrowIndex = 0;
+            }
         }
+        else if(menuKey.ReadValue<float>() > 0 && menuKey.triggered)
+        {
+            arrowIndex--;
+            if (arrowIndex < 0)
+            {
+                arrowIndex = arrowPositions.Length - 1;
+            }
+        }
+        selectMode.position = arrowPositions[arrowIndex].position;
+
+        if (menuSelect.triggered && arrowIndex == 0)
+        {
+            SceneManager.LoadScene("Lvl1");
+        }
+        else if (menuSelect.triggered && arrowIndex == 1) 
+        {
+            if (isGameOver)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+            else 
+            {
+                // TODO
+            }
+                
+        }
+        //  if (isGameOver)
     }
 
 
