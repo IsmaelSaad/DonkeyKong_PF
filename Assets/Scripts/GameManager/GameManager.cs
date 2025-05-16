fff7d20b209daa0687e6c2767e5c228f19c43dc6
PolBelRos <a24polbelros@inspedralbes.cat>
2025-05-16 20:11:10 +0200

    He añadido las escenas de dialogo y escena de EnterName (Aun no pasa de escena)

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -8,10 +8,12 @@ using UnityEngine.UIElements;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    [SerializeField] InputActionAsset inputActionPause;
+    [SerializeField] InputActionAsset inputActionPause, inputActionName;
     [SerializeField] GameObject pause, life1, life2;
-    private InputAction pauseKey;
-
+    private InputAction pauseKey, enterNameKey;
+    
+    string actualScene;
+    
     public static CoroutineRunner _instance
     {
         get
@@ -58,53 +60,66 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
 
-        pauseKey = inputActionPause.FindActionMap("InGameTools").FindAction("Pause");
-        pause.GetComponent<CanvasGroup>().alpha = 0;
+        
 
         
     }
     void Update()
     {
-        GameObject.FindGameObjectWithTag("Points").GetComponent<TMP_Text>().text = GetPoints().ToString("D6");
+        if (actualScene == "NamePlayer")
+        {
+            enterNameKey = inputActionName.FindActionMap("VerticalMenu").FindAction("SelectionMenu");
+
+            if (enterNameKey.triggered)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+
+        else if (actualScene == "Lvl1" || actualScene == "Lvl2")
+        {
+             GameObject.FindGameObjectWithTag("Points").GetComponent<TMP_Text>().text = GetPoints().ToString("D6");
        
 
-        if (lifes == 0)
-        {
-            Debug.Log("gameover");
-            SceneManager.LoadScene("GameOver");
-            ResetPoints();
-        }
+            if (lifes == 0)
+            {
+                Debug.Log("gameover");
+                SceneManager.LoadScene("GameOver");
+                ResetPoints();
+            }
 
-        switch (lifes)
-        {
-            case 2:
-                life1.GetComponent<CanvasGroup>().alpha = 0;
-                break;
-            case 1:
-                life1.GetComponent<CanvasGroup>().alpha = 0;
-                life2.GetComponent<CanvasGroup>().alpha = 0;
-                break;
-        }
+            switch (lifes)
+            {
+                case 2:
+                    life1.GetComponent<CanvasGroup>().alpha = 0;
+                    break;
+                case 1:
+                    life1.GetComponent<CanvasGroup>().alpha = 0;
+                    life2.GetComponent<CanvasGroup>().alpha = 0;
+                    break;
+            }
 
-        switch (state)
-        {
-            case State.INGAME:
-                if (pauseKey.triggered)
-                {
-                    pause.GetComponent<CanvasGroup>().alpha = 1;
-                    state = State.INPAUSE;
-                }
-                Time.timeScale = 1;
-                break;
-            case State.INPAUSE:
-                if (pauseKey.triggered)
-                {
-                    pause.GetComponent<CanvasGroup>().alpha = 0;
-                    state = State.INGAME;
-                }
-                Time.timeScale = 0;
-                break;
+            switch (state)
+            {
+                case State.INGAME:
+                    if (pauseKey.triggered)
+                    {
+                        pause.GetComponent<CanvasGroup>().alpha = 1;
+                        state = State.INPAUSE;
+                    }
+                    Time.timeScale = 1;
+                    break;
+                case State.INPAUSE:
+                    if (pauseKey.triggered)
+                    {
+                        pause.GetComponent<CanvasGroup>().alpha = 0;
+                        state = State.INGAME;
+                    }
+                    Time.timeScale = 0;
+                    break;
+            }
         }
+        
     }
 
     private void OnEnable()
@@ -116,10 +131,15 @@ public class GameManager : MonoBehaviour
     {
         if (scene.name == "Lvl1" || scene.name == "Lvl2")
         {
+            pauseKey = inputActionPause.FindActionMap("InGameTools").FindAction("Pause");
+            pause.GetComponent<CanvasGroup>().alpha = 0;
+
             pause = GameObject.FindGameObjectWithTag("Pause");
             life1 = GameObject.FindGameObjectWithTag("Life1");
             life2 = GameObject.FindGameObjectWithTag("Life2");
         }
+
+        actualScene = scene.name;
     }
 
     public void AddPoints(int p) {
